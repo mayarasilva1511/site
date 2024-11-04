5932aefe3da451c162cd46931c4601b1a7ede8f8
mayara silvaa <108280341+mayarasilva1511@users.noreply.github.com>
2024-11-04 19:16:33 -0300

    a

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contato-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-    this.reset();
-});
