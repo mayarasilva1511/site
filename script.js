c4b934006d3948b18bfeb5a615f056318380b672
mayara silvaa <108280341+mayarasilva1511@users.noreply.github.com>
2024-11-04 18:57:38 -0300

    a

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-let carrinho = [];
-let total = 0;
-
-function adicionarAoCarrinho(nome, preco) {
-    carrinho.push({ nome, preco });
-    total += preco;
-    atualizarCarrinho();
-}
-
-function atualizarCarrinho() {
-    const carrinhoContador = document.getElementById('carrinho-contador');
-    const carrinhoItens = document.getElementById('carrinho-itens');
-    const totalDisplay = document.getElementById('total');
-
-    carrinhoContador.textContent = carrinho.length;
-    carrinhoItens.innerHTML = carrinho.map(item => `<p>${item.nome} - R$ ${item.preco.toFixed(2)}</p>`).join('');
-    totalDisplay.textContent = total.toFixed(2);
-}
+document.getElementById('contato-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    this.reset();
+});
